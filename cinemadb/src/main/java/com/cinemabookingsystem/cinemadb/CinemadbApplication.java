ae2ecddaa8cadb381bad876b0a245f159f66ad2b
Dylan DiPrima <dylanwd37@gmail.com>
2024-02-29 22:40:47 -0500

    Application starts successfully

--- cinemadb/src/main/java/com/cinemabookingsystem/cinemadb/CinemadbApplication.java
+++ cinemadb/src/main/java/com/cinemabookingsystem/cinemadb/CinemadbApplication.java
@@ -2,7 +2,13 @@ package com.cinemabookingsystem.cinemadb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("my.package.base.*")
+@ComponentScan(basePackages = { "my.package.base.*" })
+@EntityScan("my.package.base.*") 
 @SpringBootApplication
 public class CinemadbApplication {
 
