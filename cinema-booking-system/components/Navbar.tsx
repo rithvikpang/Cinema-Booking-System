ed1da56732b99ac489776ff890ea6ad58585c496
ameliengo1002 <thingo1002@gmail.com>
2024-03-30 15:59:57 -0400

    created separate log in pgs for admin and user with each pg generating their respective tokens

--- cinema-booking-system/components/Navbar.tsx
+++ cinema-booking-system/components/Navbar.tsx
@@ -10,19 +10,37 @@ import { useRouter } from 'next/navigation';
 const Navbar = () => {
 
   const [hospitals, setHospitals] = useState([]);
-  const [token, setToken] = useState<string | null>();
+  const [userToken, setUserToken] = useState<string | null>();
+  const [adminToken, setAdminToken] = useState<string | null>();
   const router = useRouter();
 
   useEffect(() => {
+    const userTkn = localStorage.getItem("userToken");
+    const adminTkn = localStorage.getItem("adminToken");
+
+    // If user token exists, assign value to token
+    if (userTkn) {
+      setUserToken(userTkn);
+    }
+
+    // If admin token exists, assign value to token
+    if (adminTkn) {
+      setAdminToken(adminTkn);
+    }
+
+  }, []);
+
+
+  /**
+    useEffect(() => {
     const storedToken = localStorage.getItem("token");
 
     // If token exists, assign value to token
     if (storedToken) {
       setToken(storedToken);
-      console.log("Token value:", storedToken); // Display token value in console
     }
   }, []);
-
+   */
 
   /**
    useEffect(() => {
@@ -40,11 +58,12 @@ const Navbar = () => {
    */
 
   const handleLogout = () => {
-    localStorage.removeItem("token");
+    localStorage.removeItem("userToken");
+    localStorage.removeItem("adminToken");
     window.location.replace("/log-out");
   };
 
-    if (!token) {
+    if (userToken) {
       return (
         <div className="navbar">
           <div className="navbar">
@@ -68,18 +87,52 @@ const Navbar = () => {
               </Link>
             </div>
             <div className="home-btn block">
-              <Link className="home-btn" href="/sign-in">
-                <button type="submit">Sign In</button>
-              </Link>
-            </div>
-            <div className="home-btn block">
-              <Link className="home-btn" href="/register">
-                <button type="submit">Register</button>
+              <Link className="home-btn" href="/">
+                <button type="button" onClick={handleLogout}>Log Out</button>
               </Link>
             </div>
           </div>
         </div>
       )
+    }
+    else if (adminToken) {
+      if (userToken) {
+        return (
+          <div className="navbar">
+            <div className="navbar">
+              <Link href="/">
+                <Image
+                className="navbar"
+                src="/camera.png"
+                width={100}
+                height={100}
+                alt="camera"
+                />
+              </Link>
+              <Link className="site-name" href="/">
+                <h1 className="navbar">Cinema Booking</h1>
+              </Link>
+            </div>
+            <div className="home-buttons">
+              <div className="edit-button">
+                <Link className="edit-button" href="/profile">
+                  <button className="edit-button">Profile</button>
+                </Link>
+              </div>
+              <div className="home-btn block">
+                <Link className="home-btn" href="/admin">
+                  <button type="button">Admin</button>
+                </Link>
+              </div>
+              <div className="home-btn block">
+                <Link className="home-btn" href="/">
+                  <button type="button" onClick={handleLogout}>Log Out</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )
+      }
     }
     else {
       return (
@@ -105,13 +158,18 @@ const Navbar = () => {
               </Link>
             </div>
             <div className="home-btn block">
-              <Link className="home-btn" href="/admin">
-                <button type="button">Admin</button>
+              <Link className="home-btn" href="/user-sign-in">
+                <button type="submit">User Sign In</button>
               </Link>
             </div>
             <div className="home-btn block">
-              <Link className="home-btn" href="/">
-                <button type="button" onClick={handleLogout}>Log Out</button>
+              <Link className="home-btn" href="/admin-sign-in">
+                <button type="submit">Admin Sign In</button>
+              </Link>
+            </div>
+            <div className="home-btn block">
+              <Link className="home-btn" href="/register">
+                <button type="submit">Register</button>
               </Link>
             </div>
           </div>
---
+++ cinema-booking-system/src/app/admin-sign-in/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState('');
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSignIn = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        localStorage.setItem('adminToken', data.data.adminToken); // Storing the JWT token, make sure this is data.data
+        console.log('Login successful:', data);
+        // const destination = data.isAdmin ? "/home" : "/home";
+        // window.location.href = destination;
+        window.location.replace("/");
+
+      } else {
+        console.error('Login failed:', data.data.message);
+        setLoginError(data.data.message);
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      setLoginError('An error occurred during login. Please try again later.');
+    }
+  };
+  
+
+  return (
+    <form className="container" onSubmit={handleSignIn}>
+      <h1>Admin Sign In</h1>
+      <div className="email block">
+        <label htmlFor="frm-email">Email</label>
+        <input
+          id="frm-email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+          autoComplete="email"
+          required
+        />
+      </div>
+      <div className="password block">
+        <label htmlFor="frm-password">Password</label>
+        <input
+          id="frm-password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+          autoComplete="current-password"
+          required
+        />
+      </div>
+      {loginError && <div className="error-message">{loginError}</div>}
+      <div className="sign-in button block">
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+}
