d721ebd83e5ed5fa78ac53d339a4b2dc95d275c5
ameliengo1002 <thingo1002@gmail.com>
2024-03-30 20:25:49 -0400

    finished most of edit profile and created change password pg

--- cinema-booking-system/src/app/profile/page.tsx
+++ cinema-booking-system/src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import Link from "next/Link"
  
 interface UserProfile {
   email: string;
@@ -116,7 +117,8 @@ const ProfilePage: React.FC = () => {
  
   return (
     <form onSubmit={handleSubmit} className="container">
-      <h1>Edit Profile</h1>
+      <h1>Profile</h1>
+      <h2>Account Information</h2>
       <div className="first-name block">
         <label htmlFor="firstname">First Name</label>
         <input
@@ -139,23 +141,150 @@ const ProfilePage: React.FC = () => {
           required
         />
       </div>
+      <div className="age block">
+        <label htmlFor="lastname">Age</label>
+        <input
+          id="age"
+          type="text"
+          name="age"
+          value={profile.age}
+          onChange={handleChange}
+          required
+        />
+      </div>
       {/* Include other fields as needed */}
       <div className="email block">
         <label htmlFor="email">Email</label>
         <input
           id="email"
-          type="email"
+          type="text"
           name="email"
           value={profile.email}
           readOnly // Make the email field read-only
         />
       </div>
-      {/* Include other fields as needed */}
+      <div className="left-button block">
+        <Link href="/change-password">
+          <button type="submit">Change Password</button>
+        </Link>
+      </div>
+      <div className="address block">
+        <label htmlFor="email">Address</label>
+        <input
+          id="address"
+          type="text"
+          name="address"
+          value={profile.address}
+          readOnly // Make the email field read-only
+        />
+      </div>
+      <div className="loc block">
+        <div className="city block">
+          <label htmlFor="email">City</label>
+          <input
+            id="city"
+            type="text"
+            name="city"
+            value={profile.city}
+            readOnly // Make the email field read-only
+          />
+        </div>
+        <div className="state block">
+          <label htmlFor="email">State</label>
+          <input
+            id="state"
+            type="text"
+            name="state"
+            value={profile.state}
+            readOnly // Make the email field read-only
+          />
+        </div>
+        <div className="zip block">
+          <label htmlFor="email">Zip Code</label>
+          <input
+            id="zip-code"
+            type="text"
+            name="zip-code"
+            value={profile.zip}
+            readOnly // Make the email field read-only
+          />
+        </div>
+      </div>
+      <div className="check-boxes">
+        <div>
+          <input type="checkbox" name="myCheckbox"/>
+          <label>Register for Promotions</label>
+        </div>
+        <div>
+          <input className="check" type="checkbox" name="myCheckbox"/>
+          <label>Remember Me</label>
+        </div>
+      </div>
+      <h2>Cards:</h2>
+      <div className="cards">
+        <h3>XXXX-XXXX-XXXX-1234</h3>
+        <div className="delete block">
+            <button type="submit">Delete</button>
+        </div>
+      </div>
+      <div>
+        <h2>Add Card</h2>
+      </div>
+      <div className="name block">
+        <label htmlFor="frm-name">Name on Payment Card</label>
+        <input
+          id="frm-name"
+          type="text"
+          name="name"
+          autoComplete="name"
+          required
+        />
+      </div>
+      <div className="card-num block">
+        <label htmlFor="frm-card-num">Card Number</label>
+        <input
+          id="frm-card-num"
+          type="card-num"
+          name="text"
+          autoComplete="card-num"
+          required
+        />
+      </div>
+      <div className="card-info block">
+        <div>
+          <label htmlFor="frm-exp">State</label>
+          <input
+            id="frm-exp"
+            type="text"
+            name="exp"
+            autoComplete="exp"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="frm-cvv">Zip Code</label>
+          <input
+            id="frm-cvv"
+            type="text"
+            name="cvv"
+            autoComplete="cvv"
+            required
+          />
+        </div>
+      </div>
       <div className="button block">
-        <button type="submit">Save Changes</button>
+        <button type="submit">Add Card</button>
+      </div>
+      <div className="left-button block">
+        <div className="button block">
+            <button type="submit">Cancel</button>
+        </div>
+        <div className="button block">
+            <button type="submit">Save Changes</button>
+        </div>
       </div>
     </form>
-  );
+  )
 };
  
 export default ProfilePage;
