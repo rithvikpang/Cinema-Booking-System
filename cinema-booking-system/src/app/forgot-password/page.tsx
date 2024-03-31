fccf4d65e36269d82bde88da71967f50bb27dd44
ameliengo1002 <thingo1002@gmail.com>
2024-03-30 21:08:38 -0400

    Created change password pg

---
+++ cinema-booking-system/src/app/forgot-password/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <form className="container">
+            <h1>Forgot Password</h1>
+            <div className="email block">
+                <label htmlFor="frm-email">Email</label>
+                <input
+                id="frm-email"
+                type="email"
+                name="text"
+                autoComplete="email"
+                required
+                />
+            </div>
+            <div className="cancel-save block">
+                <button type="submit">Reset Password</button>
+            </div>
+        </form>
+    )
+}
