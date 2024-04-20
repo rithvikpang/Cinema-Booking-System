21b94971844911ab42cb66491e166293443c3ac5
ameliengo1002 <thingo1002@gmail.com>
2024-04-19 22:17:05 -0400

    Improved UI for movie info and started manage movies functionality

--- cinema-booking-system/components/ManageMovie.tsx
+++ cinema-booking-system/components/ManageMovie.tsx
@@ -65,43 +65,4 @@ const PostCard = ({ movie }: Props) => {
   );
 }
 
-/*
-import Image from 'next/image';
-import React from 'react';
-import { Movie } from '../utils/types'; // Import the type
-
-
-interface Props {
-  movie: Movie;
-}
-
-const PostCard = ({ movie }: Props) => {
-  return (
-    <div className="three-col">
-        <div className="card">
-            <Image
-            src={movie.image_url}
-            width={240}
-            height={300}
-            alt={`${movie.title} poster`}/>
-
-            <h3 className="description">{movie.title}</h3>
-            <dl>
-                <dt className="description">{movie.duration} | PG</dt>
-                <dt className="description">Released {movie.release_date}</dt>
-            </dl>
-            <div className="home-buttons block">
-                <div className="h-button block">
-                    <button type="submit">Watch Trailer</button>
-                </div>
-                <div className="h-button block">
-                    <button type="submit">Book Movie</button>
-                </div>
-            </div>
-        </div>
-    </div>
-  );
-}
-*/
-
 export default PostCard;
--- cinema-booking-system/components/MovieInfo.tsx
+++ cinema-booking-system/components/MovieInfo.tsx
@@ -7,24 +7,50 @@ interface Props {
   trailer: string;
   title: string;
   rating: string;
+  genre: number;
   cast: string;
   director: string;
   descr: string;
   imageUrl: string;
+  producer: string;
+  reviews: string;
 }
 
-const TrailerPopup: React.FC<Props> = ({ isOpen, onClose, trailer, title, rating, cast, director, descr, imageUrl}) => {
+const TrailerPopup: React.FC<Props> = ({ isOpen, onClose, trailer, title, rating, genre, cast, director, descr, imageUrl, producer, reviews}) => {
 
     const embedUrl = trailer.includes("watch?v=") 
         ? trailer.replace(/watch\?v=/, "embed/") 
         : trailer;
 
-        // Creates url string with movie info
-        const handleBookClick = () => {
-            const queryString = `?title=${encodeURIComponent(title)}&imageUrl=${encodeURIComponent(imageUrl)}`;
-            window.location.href = `/select-show-time${queryString}`;
-            onClose(); // Close the modal after navigating to the booking page
-        };
+    // Creates url string with movie info
+    const handleBookClick = () => {
+        const queryString = `?title=${encodeURIComponent(title)}&imageUrl=${encodeURIComponent(imageUrl)}`;
+        window.location.href = `/select-show-time${queryString}`;
+        onClose(); // Close the modal after navigating to the booking page
+    };
+
+    var movie_genre = null;
+
+    // Gets genre according to genre id
+    if (genre == 1) {
+        movie_genre = "Comedy"
+    }
+
+    if (genre == 2) {
+        movie_genre = "Drama"
+    }
+
+    if (genre == 3) {
+        movie_genre = "Action"
+    }
+
+    if (genre == 4) {
+        movie_genre = "Romance"
+    }
+
+    if (genre == 5) {
+        movie_genre = "Adventure"
+    }
 
     if (!isOpen) return null;
 
@@ -48,8 +74,8 @@ const TrailerPopup: React.FC<Props> = ({ isOpen, onClose, trailer, title, rating
                 flexDirection: 'row',
                 alignItems: 'center',
                 width: '70%',
-                height: '50%',
-                borderRadius: '15px'
+                height: '60%',
+                borderRadius: '25px'
                 
             }} onClick={e => e.stopPropagation()} // Prevent click inside the modal from closing it
             >
@@ -62,18 +88,20 @@ const TrailerPopup: React.FC<Props> = ({ isOpen, onClose, trailer, title, rating
                 allowFullScreen
                 style={{
                     width: '60%',
-                    height: '100%',
+                    height: '85%',
                     padding: '40px'
                 }}
                 ></iframe>
 
-                <div>
+                <div >
                     <h2 className="descr">{title}</h2>
                     <h4 className="descr">Rated {rating}</h4>
-                    <h4 className="descr">Genre: </h4>
+                    <h4 className="descr">Genre: {movie_genre}</h4>
                     <h4 className="descr">Director: {director}</h4>
+                    <h4 className="descr">Producer: {producer}</h4>
                     <h4 className="descr">Cast: {cast}</h4>
                     <h4 className="descr">Sypnosis: {descr}</h4>
+                    <h4 className="descr">Reviews: {reviews}</h4>
 
                     <div className="select-time block">
                         <button className="left-button block" onClick={handleBookClick}>Click to Book Movie</button>
