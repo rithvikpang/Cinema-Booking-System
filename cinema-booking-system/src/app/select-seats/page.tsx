32b408c4509cd0fc8c4c048bd36e68e673bf2001
EdisMake <edismakelja234@gmail.com>
2024-05-06 02:36:14 -0400

    mostly done with selecting seats and tickets, small issue with redirecting from tickets to checkout-pg2

--- cinema-booking-system/src/app/select-seats/page.tsx
+++ cinema-booking-system/src/app/select-seats/page.tsx
@@ -1,23 +1,78 @@
-import React from 'react';
-import Grid from '../../../components/SeatsGrid';
+'use client';
+import React, { useState, useEffect } from 'react';
+import SeatsGrid from '../../../components/SeatsGrid';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-const MyPage: React.FC = () => {
+const SelectSeats: React.FC = () => {
+  const router = useRouter();
+  const queryParams = new URLSearchParams(window.location.search);
+  const ticketCount = parseInt(queryParams.get('ticketCount') || '0');
+  const adultCount = parseInt(queryParams.get('adultCount') || '0');
+  const childCount = parseInt(queryParams.get('childCount') || '0');
+  const seniorCount = parseInt(queryParams.get('seniorCount') || '0');
+  const showroomId = queryParams.get('showroomId') || '';
+  const showId = queryParams.get('showId') || '';
+  const title = queryParams?.get('title') || '';
+  const date = queryParams?.get('date') || '';
+  const time = queryParams?.get('time') || '';
+
+  const [selectedSeats, setSelectedSeats] = useState<number[]>([]);
+
+  useEffect(() => {
+    // Reset selected seats when the component mounts or ticket counts change
+    setSelectedSeats([]);
+  }, [ticketCount, adultCount, childCount, seniorCount]);
+
+  const handleSeatClick = (seatIndex: number) => {
+    if (selectedSeats.length < ticketCount) {
+      if (selectedSeats.includes(seatIndex)) {
+        setSelectedSeats(selectedSeats.filter(seat => seat !== seatIndex));
+      } else {
+        setSelectedSeats([...selectedSeats, seatIndex]);
+      }
+    }
+  };
+  const handleNextButtonClick = () => {
+    const queryParams = new URLSearchParams();
+    queryParams.append('selectedSeats', selectedSeats.join(','));
+    queryParams.append('adultCount', adultCount.toString());
+    queryParams.append('childCount', childCount.toString());
+    queryParams.append('seniorCount', seniorCount.toString());
+    queryParams.append('showroomId', showroomId);
+    queryParams.append('showId', showId);
+  
+  
+    // Append the movie title, date, and time to the query parameters
+    queryParams.append('title', title);
+    queryParams.append('date', date);
+    queryParams.append('time', time);
+  
+    router.push(`/tickets?${queryParams.toString()}`);
+  };
+
   return (
-    <div className="log-out-container">
+    <div className="container">
+      <h1>Select Seats</h1>
       <div className="seat-grid">
-        <h1>Select Seats</h1>
-        <div className="seats">
-          <Grid rows={10} cols={10} />
-        </div>
+        <SeatsGrid
+          rows={10}
+          cols={10}
+          onSeatClick={handleSeatClick}
+          selectedSeats={selectedSeats}
+        />
       </div>
-      <div className="next-button block">
-        <Link href="/tickets">
-          <button type="button">Next</button> 
-        </Link>
-      </div>  
-    </div>    
+      <div className="button block">
+        <button
+          type="button"
+          onClick={handleNextButtonClick}
+          disabled={selectedSeats.length !== ticketCount}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default MyPage;
+export default SelectSeats;
--- cinema-booking-system/src/app/select-show-time/page.tsx
+++ cinema-booking-system/src/app/select-show-time/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import ShowTimeSelection from '../../../components/ShowTimeSelection';
 
@@ -27,9 +27,6 @@ interface Show {
 }
 
 const SelectShowTime: React.FC<Movie> = () => {
-  const [token, setToken] = useState<string | null>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>('');
   const [buttonClicked, setButtonClicked] = useState(false);
   const router = useRouter();
 
@@ -64,34 +61,16 @@ const SelectShowTime: React.FC<Movie> = () => {
   };
 
   const handleNextButtonClick = (selectedShowTime: string | null, showroomId: number, showId: number) => {
-      if (selectedShowTime) {
-        // Append showroomId and showId to the URL
-        router.push(`/select-tickets?selectedShowTime=${encodeURIComponent(selectedShowTime)}&showroomId=${showroomId}&showId=${showId}`);
+    if (selectedShowTime) {
+      const selectedShow = showsFromQueryParams.find(show => `${show.date} ${show.time}` === selectedShowTime);
+      if (selectedShow) {
+        const { date, time } = selectedShow;
+        // Append showroomId, showId, title, date, and time to the URL
+        router.push(`/select-tickets?selectedShowTime=${encodeURIComponent(selectedShowTime)}&showroomId=${showroomId}&showId=${showId}&title=${encodeURIComponent(title || '')}&date=${encodeURIComponent(date)}&time=${encodeURIComponent(time)}`);
       }
+    }
   };
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-
-    // If token exists, assign value to token
-    if (storedToken) {
-      setToken(storedToken);
-    }
-
-    const fetchProfile = async () => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            router.push('/log-in-redir'); // Does not allow non-logged in users to access this page
-            setError('No token found in localStorage');
-            setLoading(false);
-            return;
-        }
-
-    };
-
-    fetchProfile();
-  }, []);
-  
   return (
     <div className="container">
       <form className="select-time block">
